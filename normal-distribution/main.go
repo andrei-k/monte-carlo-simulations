3220c9e171b074fa7f74ea428c09de9e99cd6139
Andrei K <andrei@12ak.com>
2022-06-20 21:12:18 -0400

    Improve comments

--- normal-distribution/main.go
+++ normal-distribution/main.go
@@ -13,21 +13,23 @@ import (
 	"gonum.org/v1/plot/vg"
 )
 
+// Creates a histogram plot with the given number of samples.
 func histPlot(samples int) {
-	// Note that rand.NormFloat64() uses a shared global object with a Mutex lock on it. Therefore, it shouldn't be used in the main goroutine. Instead, allow each CPU to generate random values inside the goroutine.
+	// Note that rand.NormFloat64() uses a shared global object with a Mutex lock on it.
+	// Therefore, it shouldn't be used in the main goroutine.
+	// Instead, allow each CPU to generate random values inside the goroutine.
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 
 	var values plotter.Values
 	var p *plot.Plot = plot.New()
 	p.Title.Text = fmt.Sprintf("%d samples", samples)
 
-	// Generate a normally distributed float64
+	// Generates a normally distributed float64.
 	var randValue float64
 
-	// Create a slice of random normally distributed values
+	// Creates a slice of random normally distributed values.
 	for i := 0; i < samples; i++ {
-		// Use a mean of 50 and a standard deviation of 5
-		// NormFloat64()*desiredStdDev + desiredMean
+		// Uses a mean of 50 and a standard deviation of 5.
 		randValue = r.NormFloat64()*5 + 50
 		values = append(values, randValue)
 	}
@@ -54,7 +56,7 @@ func main() {
 	var samples float64
 
 	for i := 1; i <= repeat; i++ {
-		// Start with 10 samples and go up to 100,000
+		// Starts with 10 samples and go up to 100,000.
 		samples = math.Pow(10, float64(i))
 		go histPlot(int(samples))
 		wg.Add(1)
