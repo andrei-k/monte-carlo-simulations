cbcf907bcccb65ed1aaf9b123902fa353a86031d
Andrei K <andrei@12ak.com>
2022-06-20 22:07:54 -0400

    Improve comments

--- calculate-pi/main.go
+++ calculate-pi/main.go
@@ -10,11 +10,15 @@ import (
 )
 
 func checkInsideCircle(samples int) (inside int) {
+	// Note that rand.NormFloat64() uses a shared global object with a Mutex lock on it.
+	// Therefore, it shouldn't be used in the main goroutine.
+	// Instead, allow each CPU to generate random values inside the goroutine.
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
 	for i := 0; i < samples; i++ {
-		// Generate a random x and y value between 0.0 and 1.0
+		// Generates random x and y values between 0.0 and 1.0.
 		x, y := r.Float64(), r.Float64()
-		// Use the equation of a unit circle to check if the generated random sample fell inside the circle
+		// Uses the equation of a unit circle to check if the generated random sample fell inside the circle.
 		if (x*x + y*y) < 1 {
 			inside++
 		}
@@ -22,38 +26,39 @@ func checkInsideCircle(samples int) (inside int) {
 	return inside
 }
 
+// calcPiSequential calculates Pi without concurrency.
 func calcPiSequential(samples int) float64 {
 	inside := checkInsideCircle(samples)
 
-	// Divide the count of samples inside the circle by total samples, which should equal to roughly π/4
+	// Divides the count of samples inside the circle by total samples, which should equal to roughly π/4.
 	ratio := float64(inside) / float64(samples)
 
-	//  Multiply by 4 to get our final estimate for π
+	//  Multiplies by 4 to get our final estimate for π.
 	return ratio * 4
 }
 
-// Calculate Pi using concurrent goroutines
+// calcPiConcurrent calculates Pi using concurrent goroutines.
 func calcPiConcurrent(samples int) float64 {
-	// Determine the number of CPUs
+	// Determines the number of CPUs.
 	var cpus = runtime.NumCPU()
 
-	// Split up the samples evenly among the CPUs
+	// Splits up the samples evenly among the CPUs.
 	threadSamples := samples / cpus
 
-	// Create a channel to receive the results from each goroutine
+	// Creates a channel to receive the results from each goroutine.
 	results := make(chan float64)
 
-	// Create a goroutine for each CPU
+	// Creates a goroutine for each CPU.
 	for i := 0; i < cpus; i++ {
 		go func() {
 			inside := checkInsideCircle(threadSamples)
-			// Send result to the channel
+			// Sends result to the channel.
 			results <- float64(inside) / float64(threadSamples) * 4
 		}()
 	}
 
 	var total float64
-	// Receive the results from each goroutine
+	// Receives the results from each goroutine.
 	for i := 0; i < cpus; i++ {
 		total += <-results
 	}
@@ -61,17 +66,17 @@ func calcPiConcurrent(samples int) float64 {
 	return total / float64(cpus)
 }
 
-// Render result to console
+// displayOutput renders the result on the console.
 func displayOutput(samples int, result float64) {
 	fmt.Print("Samples: ", samples)
-	// Adjust spaces for formatting
+	// Adjusts spaces for formatting.
 	spaces := 12 - int(math.Log10(float64(samples)))
 	fmt.Printf("%s", strings.Repeat(" ", spaces))
 	fmt.Println("Pi:", result)
 }
 
 func main() {
-	// Start with 10 samples and go up to 100,000,000
+	// Starts with 10 samples and go up to 100,000,000.
 	iterations := 8
 	var samples float64
 
--- calculate-pi/main_test.go
+++ calculate-pi/main_test.go
@@ -6,14 +6,14 @@ import (
 
 const samples = 100000000
 
-func BenchmarkCalcPISequential(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		calcPiSequential(samples)
-	}
-}
-
 func BenchmarkCalcPiConcurrent(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		calcPiConcurrent(samples)
+	}
+}
+
+func BenchmarkCalcPISequential(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		calcPiSequential(samples)
 	}
 }
